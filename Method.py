da74598021a9ec91aed1ac05b2ebbc44e01686b1
RaphaelOduory <raphael.oduory@yahoo.com>
2016-10-28 17:30:21 +0300

    Updated Function

--- Method.py
+++ Method.py
@@ -1,9 +1,9 @@
 def prime_number(num):
 	for num in range(2, num):
 		Prime = True
-			for i in range(2,num):
+		for i in range(2,num):
 				if (num%i) == 0:
 					Prime = False
-				elif Prime:
-					print(num)
-print prime_number(11)
+		if Prime:
+				print(num)
+prime_number(11)
